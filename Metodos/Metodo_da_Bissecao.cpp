f4895ed375ac279f944d7c9bee080703f64e9bdb
Vinicus Sabariz <vsabariz@hotmail.com>
2019-09-06 17:42:01 -0300

    Otimização

--- Metodos/Metodo_da_Bissecao.cpp
+++ Metodos/Metodo_da_Bissecao.cpp
@@ -7,7 +7,7 @@
 int main(void){
 
   vector<double> polinomio {1, 30, 49, 12, 5, 27};
-
+ 
   cout << "Valor aproximado da raiz: " << bisection(-1000000000, 1000000000, 0.0000000000001, 100, polinomio) << "\n";
   return 0;
 }
