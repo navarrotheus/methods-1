5aadfe7233aecd319965354f42acd06229d171c9
Vinicius Sabariz <38088170+ViniciusSabariz@users.noreply.github.com>
2019-10-14 21:05:24 -0300

    Concluindo modificações no polinomio

--- Trabalho 1/polinomio.hpp
+++ Trabalho 1/polinomio.hpp
@@ -15,19 +15,17 @@ vector<double> formPolynomial();
 //Monta o polinomio de no maximo grau 4
 vector<double> formPolynomial(){
   //Cria um vetor de double vazio de 10 posicoes
-  std::vector<double> polynomial (9, 0);
-  double var;
+  vector<double> polynomial (10, 0);
   //Preenche o vetor com os dados input do usuario
+  double var;
   for(int i=0; i<5; i++){
     //Comeca do menor grau (zero) ate o maior (quatro)
-    cout << " \n Coeficiente de grau: " << i << "\n";
+    cout << "\nCoeficiente de grau: " << i << "\n";
     cin >> var;
-    //Armazena os valores do polinomio nas posicoes pares
-    //de forma que se um elemento tem grau 2 ele fica alocado na
-    //posicao 4, se tem grau 3 fica alocado na posicao 6... e assim por diante
     polynomial[i*2] = var;
   }
   //Preenche as posicoes impares do vetor com o valor da derivada de cada grau
+  
   polynomial = formDerivate(polynomial);
   return polynomial;
 }
@@ -42,17 +40,19 @@ vector<double> formDerivate(vector<double> polynomial){
 }
 
 void printPoly(vector<double> polynomial){
-  cout << "\n Polinomio P(x): ";
+  cout << "\nPolinomio P(x): ";
+  cout << polynomial[0] << " ";
   for(int i=0; i<4; i++){
-    cout << polynomial[(i+1)*2] << " ";
-  }
-  cout << "\n Derivado de P(x): ";
-  for(int i=0; i< 4; i++){
-    cout << polynomial[(i*2)+1] << " ";
-  }
-  cout << "\n Meu vetor: ";
-  for(int i=0; i< int(polynomial.size()); i++){
-    cout << polynomial[i] << " ";
+    cout << polynomial[(i+1)*2] << "x^" << i+1 << " ";
   }
   cout << "\n";
+  //cout << "\nDerivado de P(x): ";
+  //for(int i=0; i< 4; i++){
+    //cout << polynomial[(i*2)+1] << " ";
+  //}
+  //cout << "\nMeu vetor: ";
+  //for(int i=0; i< int(polynomial.size()); i++){
+    //cout << polynomial[i] << " ";
+  //}
+  //cout << "\n"; 
 }
