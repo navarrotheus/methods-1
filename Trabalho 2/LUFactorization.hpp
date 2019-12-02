f684147ec8d9f88a3d520cc9190b3075abcdd4d3
GustavoM7 <gustavosmarquesf@gmail.com>
2019-12-02 00:24:10 -0300

    estrutura de classes

--- Trabalho 2/LUFactorization.hpp
+++ Trabalho 2/LUFactorization.hpp
@@ -3,22 +3,165 @@
 
 using namespace std;
 
-//Class para fatoração L U
-//LSteps é uma lista de matrizes contendo todos os passos para encontrar L
-//LSteps[n - 1 é o resultado de L
-//USteps é uma lista de matrizes contendo todos os passos para encontrar U
-//USteps[n - 1] é o resultado de U
+//Classe para pai para Fatorização LU (Redução de Doolittle e Com pivotação)
+
+//LSteps é um lista de matriz e guarda todos os passos para encontrar L (LSteps[n-1])
+//USteps é um lista de matriz e guarda todos os passos para encontrar U (USteps[n-1])
+//b guarda vetor com variáveis livres
+//A guarda matriz inicial
+//result é um vetor com o resultado dos sistema
+//n é a ordem da matriz A
+
+//getLStep retorna um passo k na construção de L
+//getUStep retorna um passo k na construção de U
+//getResult retorna o resultado do sistema do vector result
+//findResult calcula o resultado do sistema e guarda no vector result (método a ser chamado junto com constructor)
 
 class LUFactorization {
   public:
     vector<vector<vector<double>>> LSteps;
     vector<vector<vector<double>>> USteps;
-
+    vector<double> b;
+    vector<vector<double>> A;
+    vector<double> result;
     int n;
 
-    LUFactorization (vector<vector<double>> A, int n){
+    //Método destrutor
+    ~LUFactorization(){
+      LSteps.erase(LSteps.begin(), LSteps.end());
+      USteps.erase(USteps.begin(), USteps.end());
+      b.erase(b.begin(), b.end());
+      A.erase(A.begin(), A.end());
+      result.erase(result.begin(), result.end());
+    }
 
+    vector<vector<double>> getLStep(int s){
+      return LSteps[s];
+    }
+
+    vector<vector<double>> getUStep(int s){
+      return USteps[s];
+    }
+
+    vector<double> getResult(){
+      return result;
+    }
+
+  protected:
+    void findResult(){
+      vector<vector<double>> L = LSteps[n-1];
+      vector<vector<double>> U = USteps[n-1];
+
+      //Ly = b por substituições sucessivas:
+      vector<double> Y;
+      for (int i = 0; i < n; i++){ // calculando Y
+        double soma = 0;
+        for (int j = 0; j < i; j++){
+          soma = soma + L[i][j] * Y[j];
+        }
+        Y.push_back((b[i] - soma)/L[i][i]);
+      }
+
+      //Ux = y por substituições retroativas:
+      vector<double> X(n); //double X[n];
+      X[n-1] = Y[n-1] / U[n-1][n-1];
+      for (int i = n-2; i >= 0 ; i--){ //calculando X
+        double soma = 0;
+        for (int j = i + 1; j < n; j++){
+          soma = soma + U[i][j] * X[j];
+        }
+        X[i] = (Y[i] - soma)/U[i][i]; 
+      }
+
+      result = X;
+    }
+};
+
+//Classe para redução de Doolittle
+//Filha da classe LUFactorization
+
+class Doolittle: public LUFactorization {
+  public:
+
+    Doolittle(vector<vector<double>> A, vector<double> b, int n){
       this->n = n;
+      this->A = A;
+      this->b = b;
+
+      //Iniciando vetor nulo para matriz L
+      vector<vector<double>> L(n);
+      for(int i=0; i<n; i++){
+        L[i].resize(n);
+      }
+
+      //Adicionando diagonal unitária
+      for(int i=0; i<n; i++){
+        L[i][i] = 1;
+      }
+
+      //Iniciando vetor nulo para matriz U
+      vector<vector<double>> U(n);
+      for(int i=0; i<n; i++){
+        U[i].resize(n);
+      }
+
+      //Passo inicial:
+      for(int i = 0; i < n; i++){
+        U[0][i] = A[0][i];
+      }
+
+      for(int i = 1; i < n; i++){
+        L[i][0] = A[i][0]/U[0][0];
+      }
+
+      USteps.push_back(U);
+      LSteps.push_back(L);
+
+      //Passo iterativo:
+
+      for(int k = 1; k < n; k++){
+          for(int j = k; j < n; j++){
+
+          // Lei de formação para elemento U
+          double sum = 0;
+          for(int r = 0; r < k; r ++){
+            sum = sum + (L[k][r] * U[r][j]);
+          }
+
+          U[k][j] = A[k][j] - sum;
+
+          //Lei de formação para elementos L
+          if(j + 1 < n){
+            sum = 0;
+            for(int r = 0; r < k; r ++){
+              sum = sum + (L[j+1][r] * U[r][k]);
+            }
+
+            L[j+1][k] = (A[j+1][k] - sum)/U[k][k];
+          }
+        }
+
+        //Salvando passo
+        USteps.push_back(U);
+        LSteps.push_back(L);
+      }
+
+      //Gerando resultado a partir do L e U gerados
+      findResult();
+    }
+
+};
+
+//Class para fatoração LU com pivotação (adicionar em breve)
+//Class filha da class LUFactorization
+
+class PivotedLU: public LUFactorization {
+  public:
+
+    PivotedLU(vector<vector<double>> A, vector<double> b, int n){
+      this->n = n;
+      this->A = A;
+      this->b = b;
 
       //Iniciando vetor nulo que receberá os multiplicadores
       vector<vector<double>> L(n);
@@ -31,7 +174,7 @@ class LUFactorization {
         L[i][i] = 1;
       }
 
-      double pivo;
+      double pivo; //Substituir por escolhar inteligente
       double m;
       for(int j=0; j<n; j++){
         //Pegando diagonal como pivo;
@@ -55,47 +198,9 @@ class LUFactorization {
 
       }
 
+      //Gerando resultado a partir do L e U gerados
+      findResult();
+
     };
 
-    //solução do sistema linear L(Ux) = b
-    vector<double> solucaoLU(vector<vector<double>> L, vector<vector<double>> U, vector<double> B){
-
-      vector<double> b = B;
-
-      //Ly = b por substituições sucessivas:
-      vector<double> Y;
-      for (int i = 0; i < n; i++){ // calculando Y
-        double soma = 0;
-        for (int j = 0; j < i; j++){
-          soma = soma + L[i][j] * Y[j];
-        }
-        Y.push_back((b[i] - soma)/L[i][i]);
-      }
-
-      //Ux = y por substituições retroativas:
-      vector<double> X(n); //double X[n];
-      X[n-1] = Y[n-1] / U[n-1][n-1];
-      for (int i = n-2; i >= 0 ; i--){ //calculando X
-        double soma = 0;
-        for (int j = i + 1; j < n; j++){
-          soma = soma + U[i][j] * X[j];
-        }
-        X[i] = (Y[i] - soma)/U[i][i]; 
-      }
-      
-      return X;
-    }
-
-    vector<vector<double>> getLStep(int s){
-      return LSteps[s];
-    }
-
-    vector<vector<double>> getUStep(int s){
-      return USteps[s];
-    }
-
-    ~LUFactorization(){
-        LSteps.erase(LSteps.begin(), LSteps.end());
-        USteps.erase(USteps.begin(), USteps.end());
-    }
 };
--- Trabalho 2/teste.cpp
+++ Trabalho 2/teste.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include "LUFactorization.hpp"
-#include "Doolittle.hpp"
 
 using namespace std;
 
@@ -35,59 +34,57 @@ int main(){
     */
 
     vector<vector<double>> A = {{1, -3, 2}, {-2, 8, -1}, {4, -6, 5}};
+    vector<double> b = {11, -15, 29};
     
-    LUFactorization fatores(A, 3);
-    vector<vector<double>> L = fatores.getLStep(2);
-    vector<vector<double>> U = fatores.getUStep(2);
+    PivotedLU P(A, b, 3);
 
-    cout << "Fatoração LU:" << endl;
+    vector<vector<double>> L1 = P.getLStep(2);
+    vector<vector<double>> U1 = P.getUStep(2);
+    vector<double> X1 = P.getResult();
+
+    cout << "Fatoração LU com pivotação:" << endl;
 
     cout << "Resultado de L:" << endl;
 
-    cout << L[0][0] << " " << L[0][1] << " " << L[0][2] << endl;
-    cout << L[1][0] << " " << L[1][1] << " " << L[1][2] << endl;
-    cout << L[2][0] << " " << L[2][1] << " " << L[2][2] << endl;
+    cout << L1[0][0] << " " << L1[0][1] << " " << L1[0][2] << endl;
+    cout << L1[1][0] << " " << L1[1][1] << " " << L1[1][2] << endl;
+    cout << L1[2][0] << " " << L1[2][1] << " " << L1[2][2] << endl << endl;
 
     cout << "Resultado de U:" << endl;
 
-    cout << U[0][0] << " " << U[0][1] << " " << U[0][2] << endl;
-    cout << U[1][0] << " " << U[1][1] << " " << U[1][2] << endl;
-    cout << U[2][0] << " " << U[2][1] << " " << U[2][2] << endl;
-
-    vector<double> b = {11, -15, 29};
-
-    vector<double> X = fatores.solucaoLU(L, U, b);
+    cout << U1[0][0] << " " << U1[0][1] << " " << U1[0][2] << endl;
+    cout << U1[1][0] << " " << U1[1][1] << " " << U1[1][2] << endl;
+    cout << U1[2][0] << " " << U1[2][1] << " " << U1[2][2] << endl << endl;
 
     cout << "Resultado Xi:" <<endl;
     for (int i = 0; i < 3; i++){
-        cout << "X" << i << " = " << X[i] << endl; 
+        cout << "X" << i << " = " << X1[i] << endl; 
     }
 
     cout << "Redução Doolittle:" << endl;
 
-    Doolittle D(A, 3);
+    Doolittle D(A, b, 3);
 
-    L = D.getLStep(2);
-    U = D.getUStep(2);
+    vector<vector<double>> L2 = D.getLStep(2);
+    vector<vector<double>> U2 = D.getUStep(2);
+    vector<double> X2 = D.getResult();
 
     cout << "Resultado de L:" << endl;
 
-    cout << L[0][0] << " " << L[0][1] << " " << L[0][2] << endl;
-    cout << L[1][0] << " " << L[1][1] << " " << L[1][2] << endl;
-    cout << L[2][0] << " " << L[2][1] << " " << L[2][2] << endl;
+    cout << L2[0][0] << " " << L2[0][1] << " " << L2[0][2] << endl;
+    cout << L2[1][0] << " " << L2[1][1] << " " << L2[1][2] << endl;
+    cout << L2[2][0] << " " << L2[2][1] << " " << L2[2][2] << endl << endl;
 
     cout << "Resultado de U:" << endl;
 
-    cout << U[0][0] << " " << U[0][1] << " " << U[0][2] << endl;
-    cout << U[1][0] << " " << U[1][1] << " " << U[1][2] << endl;
-    cout << U[2][0] << " " << U[2][1] << " " << U[2][2] << endl;
+    cout << U2[0][0] << " " << U2[0][1] << " " << U2[0][2] << endl;
+    cout << U2[1][0] << " " << U2[1][1] << " " << U2[1][2] << endl;
+    cout << U2[2][0] << " " << U2[2][1] << " " << U2[2][2] << endl << endl;
 
-
-    X = D.solucaoLU(L, U, b);
 
     cout << "Resultado Xi:" <<endl;
     for (int i = 0; i < 3; i++){
-        cout << "X" << i << " = " << X[i] << endl; 
+        cout << "X" << i << " = " << X2[i] << endl; 
     }
     
     return 0;
